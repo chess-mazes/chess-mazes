168d6f87b05b8e8b9e4a64a2637a2fa2e71fddf5
gsog2009@gmail.com <gsog2009@gmail.com>
2023-03-21 12:04:53 +0200

    higher res images

--- assets.js
+++ assets.js
@@ -1,16 +1,16 @@
 const pieceImages = {
-    "P": "https://upload.wikimedia.org/wikipedia/commons/0/04/Chess_plt60.png",
-    "R": "https://upload.wikimedia.org/wikipedia/commons/5/5c/Chess_rlt60.png",
-    "N": "https://upload.wikimedia.org/wikipedia/commons/2/28/Chess_nlt60.png",
-    "B": "https://upload.wikimedia.org/wikipedia/commons/9/9b/Chess_blt60.png",
-    "Q": "https://upload.wikimedia.org/wikipedia/commons/4/49/Chess_qlt60.png",
-    "K": "https://upload.wikimedia.org/wikipedia/commons/3/3b/Chess_klt60.png",
-    "p": "https://upload.wikimedia.org/wikipedia/commons/c/cd/Chess_pdt60.png",
-    "r": "https://upload.wikimedia.org/wikipedia/commons/a/a0/Chess_rdt60.png",
-    "n": "https://upload.wikimedia.org/wikipedia/commons/f/f1/Chess_ndt60.png",
-    "b": "https://upload.wikimedia.org/wikipedia/commons/8/81/Chess_bdt60.png",
-    "q": "https://upload.wikimedia.org/wikipedia/commons/a/af/Chess_qdt60.png",
-    "k": "https://upload.wikimedia.org/wikipedia/commons/e/e3/Chess_kdt60.png"
+    "P": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Chess_plt45.svg/240px-Chess_plt45.svg.png",
+    "R": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Chess_rlt45.svg/240px-Chess_rlt45.svg.png",
+    "N": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Chess_nlt45.svg/240px-Chess_nlt45.svg.png",
+    "B": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Chess_blt45.svg/240px-Chess_blt45.svg.png",
+    "Q": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Chess_qlt45.svg/240px-Chess_qlt45.svg.png",
+    "K": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Chess_klt45.svg/240px-Chess_klt45.svg.png",
+    "p": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/240px-Chess_pdt45.svg.png",
+    "r": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Chess_rdt45.svg/240px-Chess_rdt45.svg.png",
+    "n": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Chess_ndt45.svg/240px-Chess_ndt45.svg.png",
+    "b": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Chess_bdt45.svg/240px-Chess_bdt45.svg.png",
+    "q": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Chess_qdt45.svg/240px-Chess_qdt45.svg.png",
+    "k": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Chess_kdt45.svg/240px-Chess_kdt45.svg.png"
 };
 
 export default pieceImages;
