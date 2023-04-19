980de0f8516af1f03e975e97c8de49cf548e526f
dvirberlo <dvir.berl@gmail.com>
2023-04-19 20:28:40 +0300

    feat: add "fork me on Github" banner

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,16 +1,28 @@
 import { useRef } from "react";
 import { Board } from "./Board";
-import { themeList, Theme } from "./themes/themes";
+import { themeList } from "./themes/themes";
+import { repository } from "@/../package.json";
 import "./themes/themes.css";
+import "./App.css";
 
 const App = () => {
   const theme = useRef(themeList[0]).current;
 
   return (
-    <div>
-      <Board theme={theme}></Board>
-      <p>App view</p>
-    </div>
+    <>
+      <a
+        className="github-fork-ribbon"
+        href={repository.url}
+        data-ribbon="Fork me on GitHub"
+        title="Fork me on GitHub"
+      >
+        Fork me on GitHub
+      </a>
+      <div>
+        <Board theme={theme}></Board>
+        <p>App view</p>
+      </div>
+    </>
   );
 };
 
