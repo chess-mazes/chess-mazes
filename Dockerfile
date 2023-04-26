4d5bb52fb8d6d4c93d8ace2b224c1b6817029706
EternalEclipse <gsog2009@gmail.com>
2023-04-27 01:48:55 +0300

    add server to vite config, simple Dockerfile + docker-compose

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node
+
+WORKDIR /app
+COPY package.json .
+RUN npm install
+COPY . .
+
+EXPOSE 5173
+CMD [ "npm", "run", "dev" ]
--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,12 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  }
 });
