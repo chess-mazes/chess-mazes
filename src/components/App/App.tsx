3b96cc01bef2e4657ffea9d45875ddfe7875e466
EternalEclipse <gsog2009@gmail.com>
2023-04-27 01:26:34 +0300

    save/load puzzleNum from localStorage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -30,6 +30,7 @@ const App = () => {
       boardState.id = newPuzzleNum;
       boardState.puzzle = structuredClone(puzzles[newPuzzleNum]);
       document.location.hash = newPuzzleNum.toString();
+      localStorage.setItem('puzzleNum', newPuzzleNum.toString());
       return newPuzzleNum;
     });
   }, []);
@@ -38,6 +39,12 @@ const App = () => {
     const hashPuzzleNum = parseInt(document.location.hash.slice(1));
     if (hashPuzzleNum !== puzzleNum && hashPuzzleNum >= 0 && hashPuzzleNum < puzzles.length) {
       setPuzzleNum(hashPuzzleNum);
+    } else {
+      // Check localStorage for a saved puzzleNum
+      const savedPuzzleNum = parseInt(localStorage.getItem('puzzleNum') ?? '-1');
+      if (savedPuzzleNum !== puzzleNum && savedPuzzleNum >= 0 && savedPuzzleNum < puzzles.length) {
+        setPuzzleNum(savedPuzzleNum);
+      }
     }
   }, []);
 
