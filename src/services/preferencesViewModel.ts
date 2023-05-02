5510d889edd9b2a7e640899099e49c609ec48174
ShahafPinto <shahafchen7@gmail.com>
2023-05-02 22:11:36 +0300

    added mp3 files

--- src/services/preferencesViewModel.ts
+++ src/services/preferencesViewModel.ts
@@ -22,12 +22,41 @@ export class PreferencesViewModel {
 
   private soundModeStorage = new StorageEntry<boolean>('soundMode', true);
   public soundMode: boolean;
+  public playlist = ['../music/Strobotone-Medieval-Theme01.mp3', '../music/Strobotone-Medieval-Theme02.mp3']
+  public currentSong = 0
+  public audio:HTMLAudioElement;
   public toggleSoundMode = () => {
     const newSoundMode = !this.soundMode;
     this.soundMode = newSoundMode;
     this.soundModeStorage.set(newSoundMode);
-  };
 
+    if(this.soundMode){
+        if (this.audio) {
+            this.audio.pause();
+        }
+        console.log("start");
+        this.currentSong = 0;
+        this.playNext(this.playlist.length)
+    }else{
+        if (this.audio) {
+            this.audio.pause();
+        }
+    }
+  };
+  public playNext = (length:number)=> { 
+    // console.log(this._playlist.length)
+    if (this.currentSong < length) { 
+      this.audio = new Audio(require(this.playlist[this.currentSong])); 
+      this.audio.addEventListener("ended", ()=>{
+        this.playNext(length)}); 
+      this.audio.play(); 
+      console.log(`playing ${this.playlist[this.currentSong]}`); 
+      this.currentSong += 1; 
+    } else { 
+        this.currentSong = 0
+        this.playNext(length)
+    } 
+  }
   constructor() {
     makeAutoObservable(this);
     this.themeMode = this.themeModeStorage.get();
