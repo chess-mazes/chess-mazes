9d611ab61cd08d3d7b0197ac0d200afb3fd762a9
dvirberlo <dvir.berl@gmail.com>
2023-05-02 17:16:09 +0300

    refactor: remove another unused hook

--- src/hooks/useForceUpdate.ts
+++
@@ -1,9 +0,0 @@
-import {useCallback, useState} from 'react';
-
-export const useForceUpdate = () => {
-  const [, setTick] = useState(0);
-  const update = useCallback(() => {
-    setTick((tick) => tick + 1);
-  }, []);
-  return update;
-};
